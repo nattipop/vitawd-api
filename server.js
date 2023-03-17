6ff25e3074eef844cdaa7a27299ab7bdd82329a6
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-03-17 18:09:37 -0500

    connection string update

--- server.js
+++ server.js
@@ -144,4 +144,4 @@ app.post("/api/new-email", (req, res) => {
   }
 })
 
-app.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000"))
+app.listen(process.env.PORT || 5000, () => console.log("Server running"))
