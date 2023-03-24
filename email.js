478a7fd6f72f7475fbf7721a6eb2c064eb9edcb4
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-03-24 10:32:08 -0500

    nodemailer config

--- email.js
+++ email.js
@@ -1,14 +1,15 @@
 const nodemailer = require("nodemailer");
 const keys = require("./config/keys");
+const nodeMailerPrivateKey = keys.nodemailer.service_account.private_key.replace(/\\n/g, "\n");
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
   auth: {
     type: "OAuth2",
-    user: "vita-web-design@imap-api-381517.iam.gserviceaccount.com",
-    serviceClient: "107845153689526738568",
-    privateKey: "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCP2s3gR1qLom7r\n8p/Ik8XcTpR1pX2YnUQ/duEJzNyEtZVQtnXaWTSuaVRZZNgU8fTv5lbUVsHYBCCM\nFipaZ9hQ6BGqdQA723CUwk21ziQ7+1CPprB+QhnbmAeZlwWFl/QP+Q+BB70OWcyh\no1ihhkO0TEhjP+uNwARg3vs7XYo2mcf4ykUqef10vGgatO7rKSf7OHKyIrCEO+no\n0nQkcydr2rNQ5SlNbOIQd5IN2R+RdUeON7xyLESmjNbx2zn3/JquA2IaisbjinlI\nNbacY9ijxJHSJ4PCkt449aQitozVBaP+Dh8MuR698LZDfzLv+ho+69fC1TuPsE+x\nwRsQVU5TAgMBAAECggEABMEgiMcS8QZ/UAEG0uQ/Z9aZBYJgskiQVuCg82f36dLu\nimpdXsgP+6uzg/jXQ2nzxHICcKYlZfRM9xRMdgx0k8f3vYB8b34DLId3dgjIHcti\nkDzcHHAm1qEhryvUnOO2x0heAM+2qeDB6J2zcjMzzaOvyMf2CwtLmcAE4aNnzAIg\nbIKunMr0xlzXBvKlDMAX5RrYnzMHc7pJnUl/ecn0IK9uUHEFz0OGujlr03DvdQjU\nBnEYHk8HfqtuIYywr4NFDOn8PGOL1rIiVN4jnQ+IZHPIZEy6NE/sDFUn6T78uNl7\nZ2FoYQD9i1P9St35gB3ftfiAJ/M03IJq0PyABr98YQKBgQDJm0211Bj7QK/YW8C1\n0BrWy1Ev8OxBT7FxM8D8ddGRONmXfEI1WCaK5oMfPXreX79wc1p+hpJNr9ccSTov\nqYPRPTDPxB0NL2yohOx2lvI8OnhotopRABm8a/vaSKwaGQOChCeFVcYHefCf8AvK\nW8yziJs6b6FPVx+yBhNR5Weg2wKBgQC2qqb5xUrXBrWlSBfqsunWmbbVxynUSV+H\nLUEgTkFjxlJOMdlKcIHozb+6ztwjpnLRwSTMhz2QLH7LU29fYSayQPOHa34CPmb4\n1D/5cCb6upd3Vwy10/NMUa3hrbtDtsipDnz3gk+v7gNSvPaOY46S7wVuO2dblvdI\n4WsBwGDl6QKBgByiLhtREGs++Lql7WzlLaqsHc/NTADKrM0DKLmykh0qEd1uDIk/\nB9paeu0I8lZ7CtmRyJyHyGe4PbkKvXTfgpVJN5GKrQWeYrRcjKkbLzRWqZtVsbdn\naEBWpeKOx8K9Ir56vpI1aHbPXtmYYlRPTXeCQ2m9SQ/Xs+y46C2Ng3ZZAoGAUKgl\ntqAhHbU+u86LkPyTOEkDbH3WwH/tiHi5CxTC1tWZPiHQuofXNP60rD3vAmbayDeq\nkg0dZ+3dxtX+lXUGays23RLfvwni2fiOjwuwioGdo/pyCE+gXJCsYLwfthSdodhd\n4cROf3ZvTTdW/ySCreqQ0U21o8GTS0ENyOYzYxECgYEAudvgVHt6kaWl7o1xwljc\nAVtvlfmGwauWUJ9V034REUwHiUuqCL8vidLf+XgRbcxgktVx2ml4eZUkj7yCKc1S\nHIt2dHn7aRSSYRdoE7Mk/SoBdXlJEDpp11pP7g+3g1qWez2Hz2Ox9wyFx3tt9YRo\nVbGDaRqBnVSWC2Q30mGf1Hg="
+    user: keys.nodemailer.service_account.client_email,
+    serviceClient: keys.nodemailer.service_account.client_id,
+    privateKey: nodeMailerPrivateKey
   }
 });
 
