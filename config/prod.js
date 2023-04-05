4b804ad77502d6c7a938a6b83b47b148fd57336d
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-04-04 19:19:30 -0500

    refreshtoken

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   senderEmail: "nattipop815@gmail.com",
   clientId: "289087365218-r3kn5lr4gov3sphd4it2b5rivpth6csu.apps.googleusercontent.com",
   clientSecret: "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",
-  refreshToken: "1//04SApAaA1Bz9ACgYIARAAGAQSNwF-L9IrYB7CdBlFulvalFkidISCInCmYAp2f4tenDzpLU9_UlmT23wjO_hLXwvaT_DIxTRBqoU"
+  refreshToken: "1//04ddgp2ClS8UaCgYIARAAGAQSNwF-L9IreYxLhdUXNadAhniQDGFQas0cnbayL90XLUPzs7dnNzt-ORbbfqQkBSlQNoGlohPB21Q"
 }
